82f31df2d073fa41ec9cb83ed3a028d9d3cbc713
Vladimir <vladimir@sobaka.com>
2024-08-18 16:43:26 +0200

    container storage

--- app/app.php
+++ app/app.php
@@ -1,13 +1,15 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 
-use Bicycle\Core\Helpers\StartHelper;
+use Anodio\Core\Helpers\StartHelper;
+
+require __DIR__ . '/vendor/autoload.php';
 
 $helper = new StartHelper();
 $helper->preload(__DIR__);
 
-$manager = new \Bicycle\Core\ContainerManagement\ContainerManager();
+$manager = new \Anodio\Core\ContainerManagement\ContainerManager();
 $container = $manager->initContainer();
+\Anodio\Core\ContainerStorage::init();
 
 $container->get('application')->run();
 
