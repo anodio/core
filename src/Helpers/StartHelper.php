392bd3baf11d12b4d4a9071000e52d2eafba01fb
Vladimir Zakotnev <vladitot@gmail.com>
2024-08-18 23:40:47 +0200

    Update StartHelper.php

--- src/Helpers/StartHelper.php
+++ src/Helpers/StartHelper.php
@@ -5,20 +5,20 @@ namespace Anodio\Core\Helpers;
 class StartHelper
 {
     public function preload($currentDirectory) {
-        define('SYSTEM_PATH', $currentDirectory.'/system');
+        if (!defined('SYSTEM_PATH')) define('SYSTEM_PATH', $currentDirectory.'/system');
         $this->createSystemDirectory(SYSTEM_PATH);
-        define('BASE_PATH', $currentDirectory);
-        define('APP_PATH', $currentDirectory.'/app');
-        define('VENDOR_PATH', $currentDirectory.'/vendor');
-        define('CONTAINER_NAME', getenv('CONTAINER_NAME') ?: 'cli');
+        if (!defined('BASE_PATH')) define('BASE_PATH', $currentDirectory);
+        if (!defined('APP_PATH')) define('APP_PATH', $currentDirectory.'/app');
+        if (!defined('VENDOR_PATH')) define('VENDOR_PATH', $currentDirectory.'/vendor');
+        if (!defined('CONTAINER_NAME')) define('CONTAINER_NAME', getenv('CONTAINER_NAME') ?: 'cli');
     }
 
     private function createSystemDirectory($path)
     {
         if (!file_exists($path)) {
             mkdir($path, 0777, true);
+            file_put_contents($path.'/.gitignore', "*\n!.gitignore");
         }
-        file_put_contents($path.'/.gitignore', "*\n!.gitignore");
     }
 
 }
