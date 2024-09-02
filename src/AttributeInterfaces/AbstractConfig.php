096f201198c6d19de7e424fadefee581b3272404
Vladimir <vladimir@sobaka.com>
2024-09-02 16:48:39 +0200

    bool config values fixed

--- src/AttributeInterfaces/AbstractConfig.php
+++ src/AttributeInterfaces/AbstractConfig.php
@@ -3,13 +3,24 @@
 namespace Anodio\Core\AttributeInterfaces;
 
 use DI\Attribute\Inject;
+use Dotenv\Dotenv;
 
 abstract class AbstractConfig
 {
+
+    #[Inject]
+    protected Dotenv $dotenv;
+
     public function __construct(array $data = [])
     {
         foreach ($data as $key=>$value) {
-            $this->$key = $value;
+            if ($value=='false') {
+                $this->$key = false;
+            } elseif ($value=='true') {
+                $this->$key = true;
+            } else {
+                $this->$key = $value;
+            }
         }
     }
 }
