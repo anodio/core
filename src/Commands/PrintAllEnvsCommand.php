b21c9aefa70af1350eac9fe8ce55b7c9e0a7e86c
Vladimir <vladimir@sobaka.com>
2024-09-02 16:49:03 +0200

    printing all possible ENVs added

---
+++ src/Commands/PrintAllEnvsCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Anodio\Core\Commands;
+
+use Anodio\Core\Configuration\Env;
+use Anodio\Core\Configuration\EnvRequired;
+use Anodio\Core\Configuration\EnvRequiredNotEmpty;
+use olvlvl\ComposerAttributeCollector\Attributes;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[\Anodio\Core\Attributes\Command('env:print-all', description: 'Print all possible envs')]
+class PrintAllEnvsCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $printed = [];
+        $targets = Attributes::findTargetProperties(Env::class);
+        foreach ($targets as $target) {
+            if (!$target->attribute instanceof Env) {
+                continue;
+            }
+            if (in_array($target->attribute->name, $printed)) {
+                continue;
+            }
+            $this->printEnv($output, $target);
+            $printed[] = $target->attribute->name;
+        }
+
+        $targets = Attributes::findTargetProperties(EnvRequiredNotEmpty::class);
+        foreach ($targets as $target) {
+            if (!$target->attribute instanceof EnvRequiredNotEmpty) {
+                continue;
+            }
+            if (in_array($target->attribute->name, $printed)) {
+                continue;
+            }
+            $this->printEnv($output, $target);
+            $printed[] = $target->attribute->name;
+        }
+
+        $targets = Attributes::findTargetProperties(EnvRequired::class);
+        foreach ($targets as $target) {
+            if (!$target->attribute instanceof EnvRequired) {
+                continue;
+            }
+            if (in_array($target->attribute->name, $printed)) {
+                continue;
+            }
+            $this->printEnv($output, $target);
+            $printed[] = $target->attribute->name;
+        }
+
+        return 0;
+    }
+
+    private function printEnv(OutputInterface $output, \olvlvl\ComposerAttributeCollector\TargetProperty $target)
+    {
+        if ($target->attribute->comment) {
+            $output->writeln('# '.$target->attribute->comment);
+        }
+        if ($target->attribute->default) {
+            if (str_contains($target->attribute->default, ' ')) {
+                $output->writeln($target->attribute->name.'="'.$target->attribute->default.'"');
+            } else {
+                $output->writeln($target->attribute->name.'='.$target->attribute->default);
+            }
+        } else {
+            $output->writeln($target->attribute->name.'=');
+        }
+    }
+}
